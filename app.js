7d6b08f9fe5089d21be5af6a59143693d9faab9c
KentFletcher <kentfletch09@gmail.com>
2021-02-22 17:45:02 -0800

    Installed method-override using npm, required and specified the way to use in app.js. Added a GET route to render a form to update info for a specific single campground and populated the input fields with the current values.  Aded a link to exit back to the show, and one to go back to all campsites.  Added a PUT route that finds and updated a specific instance in the db with the new data.  Then redirect back to the show page for that specific newly updated campsite.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campgrounds')
+const Campground = require('./models/campgrounds');
+const methodOverride = require('method-override');
 
 //Connect to mongo DB using mongoose
 mongoose.connect('mongodb://localhost:27017/yelpCamp', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
@@ -18,6 +19,7 @@ db.once('open', () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 //**ROUTES**
 app.get('/', (req, res) => {
@@ -29,13 +31,14 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { camps });
 })
 
-//Create a new campground
+//CREATE a new campground
 //serve form to create the new campground
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 
-//post route where the form from new route will be submitted. Then create the new campground, insert into the database, then redirect and display the newly created instance 
+
+//POST route where the form from new route will be submitted. Then create the new campground, insert into the database, then redirect and display the newly created instance 
 app.post('/campgrounds', async (req, res) => {
   const newCampground = new Campground(req.body.campground);
   await newCampground.save();
@@ -47,6 +50,21 @@ app.get('/campgrounds/:id', async (req, res) => {
   const { id } = req.params;
   const camp = await Campground.findById(id);
   res.render('campgrounds/show', { camp })
+})
+
+//UPDATE 
+//GET route to render the new form for updating the information
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const camp = await Campground.findById(id);
+  res.render('campgrounds/edit', { camp })
+})
+
+//PUT route for changing/updating data about a single/specific campground, then insert into the database, and then redirect and display the newly updated instance 
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const updatedCamp = await Campground.findByIdAndUpdate(id, req.body.campground, { runValidators: true, new: true });
+  res.redirect(`/campgrounds/${updatedCamp._id}`)
 })
 
 //start server listening for requests
