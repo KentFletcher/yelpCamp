8cf1a623b9c0338487efe940c2d0ff7880b7c0be
KentFletcher <kentfletch09@gmail.com>
2021-02-27 10:59:53 -0800

    created a generic error handler to get started with.  Also added a utilities directory, inside made a file with a function to wrap around all of the async functions that will handle the try/catch for all the async express functions.  Wrapped all the async functions in my routes in that catchAsyn() handler.  Also created a ExpressError.js that extends the Express Error, will use this in the near future as I expand my error handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const ejsMate = require('ejs-mate')
 const path = require('path');
 const mongoose = require('mongoose');
+const catchAsync = require('./utilities/catchAsync');
 const Campground = require('./models/campgrounds');
 const methodOverride = require('method-override');
 
@@ -28,10 +29,10 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
   const camps = await Campground.find({});
   res.render('campgrounds/index', { camps, title: 'All Campgrounds' });
-})
+}))
 
 //CREATE a new campground
 //serve form to create the new campground
@@ -41,40 +42,44 @@ app.get('/campgrounds/new', (req, res) => {
 
 
 //POST route where the form from new route will be submitted. Then create the new campground, insert into the database, then redirect and display the newly created instance 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res) => {
   const newCampground = new Campground(req.body.campground);
   await newCampground.save();
   res.redirect(`/campgrounds/${newCampground._id}`)
-})
+}))
 
 //READ/SHOW route for individual campgrounds
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const camp = await Campground.findById(id);
   res.render('campgrounds/show', { camp, title: camp.title })
-})
+}))
 
 //UPDATE 
 //GET route to render the new form for updating the information
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const { id } = req.params;
   const camp = await Campground.findById(id);
   res.render('campgrounds/edit', { camp, title: `Update ${camp.title}` })
-})
+}))
 
 //PUT route for changing/updating data about a single/specific campground, then insert into the database, and then redirect and display the newly updated instance 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const updatedCamp = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { runValidators: true, new: true });
   res.redirect(`/campgrounds/${updatedCamp._id}`)
-})
+}))
 
 //DESTROY
 //Delete route to find a specific instance of one campground and remove that camp and all its data from the database.
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds')
+}))
+
+app.use((err, req, res, next) => {
+  res.send('OH NO , SOMETHING WENT WRONG!!!!!!!')
 })
 
 //start server listening for requests
