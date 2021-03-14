e644aa4c768366e1fb5aeb0dd5c99782ecdf3d36
KentFletcher <kentfletch09@gmail.com>
2021-03-14 15:38:18 -0700

    Added mongoose middleware in the campground.js file to handle the removal of all reviews from the reviews db for a given campsite when that site is deleted.

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const campgroundSchema = new Schema({
@@ -14,6 +15,16 @@ const campgroundSchema = new Schema({
     }
   ]
 });
+
+//Middleware - Removes the reviews associated with a given campground from the DB when the campground is deleted
+//POST, after a campground is deleted this function goes into the reviews array and removes all of those reviews from the Review model in the database.  
+campgroundSchema.post('findOneAndDelete', async function (camp) {
+  if (camp.reviews.length) {
+    await Review.deleteMany({
+      _id: { $in: camp.reviews }
+    })//in operator looks within an array
+  }
+})
 
 const Campground = mongoose.model('Campground', campgroundSchema);
 module.exports = Campground;
