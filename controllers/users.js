5659b60d496ade07938c7d7c4d987faaba119bec
KentFletcher <kentfletch09@gmail.com>
2021-04-06 14:25:08 -0700

    used router.route in the campgrounds/routes.js file to group together duplicate routes, like '/' and '/:id'

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,4 @@
 const User = require('../models/user');
-
 const session = require('express-session');
 
 module.exports.renderRegisterForm = (req, res) => {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,34 +3,24 @@ const router = express.Router();
 const catchAsync = require('../utilities/catchAsync');
 const Campground = require('../models/campgrounds');
 const { campgroundSchema } = require('../schemas.js');
-const { Router } = require('express');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds')
 
-//Campground routes
-router.get('/', catchAsync(campgrounds.index))
+router.route('/')
+  .get(catchAsync(campgrounds.index))
+  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCampground))//route where the form from new route will be submitted. Then create the new campground, insert into the database, then redirect and display the newly created instance, validateCampground is the server-side error handler 
 
 //CREATE
-//serve form to create the new campground
-router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)//serve form to create the new campground
 
-//POST route where the form from new route will be submitted. Then create the new campground, insert into the database, then redirect and display the newly created instance, validateCampground is the server-side error handler 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCampground))
-
-//READ
-// SHOW route for individual campgrounds
-router.get('/:id', catchAsync(campgrounds.showCampground))
+router.route('/:id')
+  .get(catchAsync(campgrounds.showCampground))//READ/ SHOW route for individual campgrounds
+  .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground))//PUT route for editing/updating data about a single/specific campground, then insert into the database, and then redirect and display the newly updated instance 
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))//DESTROY/ Delete route to find a specific instance of one campground and remove that camp and all its data from the database.
 
 //UPDATE 
 //GET route to render the new form for updating the information
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-//PUT route for changing/updating data about a single/specific campground, then insert into the database, and then redirect and display the newly updated instance 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground))
-
-//DESTROY
-//Delete route to find a specific instance of one campground and remove that camp and all its data from the database.
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 module.exports = router;
 
