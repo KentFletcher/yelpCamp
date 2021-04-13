150c4f930bb34db174a50e164716bf96635685e7
KentFletcher <kentfletch09@gmail.com>
2021-04-13 12:30:54 -0700

    In the /campgrounds/show.ejs set variables for long and lat in the bottom script to be used in the /showPageMap.js.  Variable were used in the center property for the starting position of were the map will focus.  Also added a marker and set the long and lat to be equal to the proper coordinates, and changed the marker to red. For the campgrounds/index.ejs added in an if/else statement to display a default image stored in the clodinary account if length of images array is less than 1. Did the same on the show page, to account for if someone deletes all the images on the edit route.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -25,7 +25,6 @@ module.exports.createNewCampground = async (req, res) => {
   campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
   campground.author = req.user._id;
   await campground.save();
-  console.log(campground)
   req.flash('success', 'Thanks for adding a new campground!')
   res.redirect(`/campgrounds/${campground._id}`)
 }
--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,6 +2,15 @@ mapboxgl.accessToken = mapToken;//this is in a script in /campgrounds/show.ejs, 
 const map = new mapboxgl.Map({
   container: 'map', // container ID from /campgrounds/show.ejs
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  // center: campground.geometry.coordinates,  [lng, lat]//from the way it was demonstrated, but had some issues on the ejs side of things, also do not need entire object
+  center: [long, lat], // starting position [lng, lat]// starting position
+  zoom: 10 // starting zoom
 });
+
+const marker = new mapboxgl.Marker({
+  color: "red",
+  draggable: true
+})
+  // .setLngLat(campground.geometry.coordinates)//from the way it was demonstrated, but had some issues on the ejs side of things, also do not need entire object
+  .setLngLat([long, lat])
+  .addTo(map);
